7dc9c905666e874050b4d9f887b048106810ffa7
suraj-rai-1900 <142300552+suraj-rai-1900@users.noreply.github.com>
2024-04-07 15:14:27 +0530

    Update convert_softmax.sh

--- cedar_scripts/convert_softmax.sh
+++ cedar_scripts/convert_softmax.sh
@@ -4,6 +4,7 @@
 #SBATCH --mem=200G  
 #SBATCH --cpus-per-task=3
 
-source /home/surajrai/scratchDataTools/cedar_scripts/sourceme.sh
+source /home/surajrai/scratch/DataTools/cedar_scripts/sourceme.sh
+mkdir /home/surajrai/scratch/softmaxes_watchmal_root/
 
-python /home/surajrai/scratch/DataTools/root_utils/convert_softmax.py "/home/surajrai/scratch/softmaxes_watchmal/"
+python /home/surajrai/scratch/DataTools/root_utils/convert_softmax.py "/home/surajrai/scratch/softmaxes_watchmal/" -o "/home/surajrai/scratch/softmaxes_watchmal_root/" 
