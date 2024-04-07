e691fd6a76acfb830aa4f0e1307c35d79d8c9dc9
suraj-rai-1900 <142300552+suraj-rai-1900@users.noreply.github.com>
2024-04-07 15:08:24 +0530

    Update npz_to_hdf5_combined.sh

--- cedar_scripts/npz_to_hdf5_combined.sh
+++ cedar_scripts/npz_to_hdf5_combined.sh
@@ -8,6 +8,7 @@ source /home/surajrai/scratch/DataTools/cedar_scripts/sourceme.sh
 source /home/surajrai/scratch/env/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index h5py
+mkdir /home/surajrai/scratch/outputs_h5_combined/
 
 input_files=($(ls /home/surajrai/scratch/outputs_npz/*.npz))
 out_fpath="/home/amisery/scratch/outputs_h5_combined/iwcd_p320ka_w750m_1e17pot_2p39_wcsim_combined.h5"
