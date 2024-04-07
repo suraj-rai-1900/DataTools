433264350b230048c3dd4ebdaf4c0b16545ddb3e
suraj-rai-1900 <142300552+suraj-rai-1900@users.noreply.github.com>
2024-04-07 14:55:56 +0530

    Create npz_to_hdf5_combined.sh

---
+++ cedar_scripts/npz_to_hdf5_combined.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=rpp-blairt2k
+#SBATCH --time=0-24:30
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=3
+
+source /home/surajrai/scratch/DataTools/cedar_scripts/sourceme.sh
+source /home/surajrai/scratch/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index h5py
+
+input_files=($(ls /home/surajrai/scratch/outputs_npz/*.npz))
+out_fpath="/home/amisery/scratch/outputs_h5_combined/iwcd_p320ka_w750m_1e17pot_2p39_wcsim_combined.h5"
+
+python /home/surajrai/DataTools/root_utils/np_to_digihit_array_hdf5.py "${input_files[@]}" -o "$out_fpath"
