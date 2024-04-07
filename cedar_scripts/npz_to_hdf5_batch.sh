d8be14b08413aa14d944320d3287943e44e25042
suraj-rai-1900 <142300552+suraj-rai-1900@users.noreply.github.com>
2024-04-07 15:20:20 +0530

    Update npz_to_hdf5_batch.sh

--- cedar_scripts/npz_to_hdf5_batch.sh
+++ cedar_scripts/npz_to_hdf5_batch.sh
@@ -9,15 +9,15 @@ pip install --no-index --upgrade pip
 pip install --no-index h5py
 mkdir /home/surajrai/scratch/outputs_h5/
 
-for ((i=0; i<10; i++))
+for ((i=0; i<1; i++))
 do
 
 lb=$((1000*i + 1))  # Calculate the lower bound
 hb=$((1000*(i+1)))  # Calculate the upper bound
 
-input_file="/home/surajrai/scratch/outputs_npz/iwcd_p320ka_w750m_1e17pot_2p39_wcsim_$(printf '%05d' i).npz"
+input_file="/home/surajrai/scratch/outputs_npz/iwcd_p320ka_w750m_1e17pot_2p39_wcsim_$(printf '%05d' 1).npz"
 
-out_fpath="/home/surajrai/scratch/outputs_h5/iwcd_p320ka_w750m_1e17pot_2p39_wcsim_$(printf '%05d' i).h5"
+out_fpath="/home/surajrai/scratch/outputs_h5/iwcd_p320ka_w750m_1e17pot_2p39_wcsim_$(printf '%05d' 1).h5"
 
 python /home/surajrai/scratch/DataTools/root_utils/np_to_digihit_array_hdf5.py $input_file -o $out_fpath
 
