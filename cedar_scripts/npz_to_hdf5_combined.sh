090acb6a7ddcaa5144be55e62fcc4303c9e3569e
suraj-rai-1900 <142300552+suraj-rai-1900@users.noreply.github.com>
2024-04-08 09:40:32 +0530

    Update npz_to_hdf5_combined.sh

--- cedar_scripts/npz_to_hdf5_combined.sh
+++ cedar_scripts/npz_to_hdf5_combined.sh
@@ -13,4 +13,4 @@ mkdir /home/surajrai/scratch/outputs_h5_combined/
 input_files=($(ls /home/surajrai/scratch/outputs_npz/*.npz))
 out_fpath="/home/amisery/scratch/outputs_h5_combined/iwcd_p320ka_w750m_1e17pot_2p39_wcsim_combined.h5"
 
-python /home/surajrai/DataTools/root_utils/np_to_digihit_array_hdf5.py "${input_files[@]}" -o "$out_fpath"
+python /home/surajrai/scratch/DataTools/root_utils/np_to_digihit_array_hdf5.py "${input_files[@]}" -o "$out_fpath"
